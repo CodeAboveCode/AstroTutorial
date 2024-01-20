3c27b0125e7b97de1963f36f2d4475bfcf414058
Code AboveCode <codeabovecode@outlook.com>
2024-01-20 15:51:04 +0000

    extend view transition

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://localhost:4321",
-  integrations: [preact()]
+  integrations: [preact(),tailwind()]
 });
